3f3a6d229baf05e8fe36f73df96fcf7a8a65e625
Arvid Larsson <arvla626@student.liu.se>
2019-06-04 10:03:40 +0200

    Some changes, now working

--- prodeacc.jl
+++ prodeacc.jl
@@ -7,17 +7,17 @@ k_pc = 0.0307   # Rate periphiral --> central compartement
 k_ei = 1.45     # Rate clearence from central compartement
 a_ka = 0.118
 b_ka = 0.698
-G    = 0.1
+G    = 1
 
 g = @ode_def begin
     dP = k_cp * C - k_pc * P
     dC = k_pc * P - k_cp* C - k_ei * C + a_ka * G
-    #dG = -a_ka * G + 1
-end k_cp k_pc k_ei a_ka G
+    dG = -a_ka * G
+end k_cp k_pc k_ei a_ka
 
-u0 = [0.0;0.0;0]
-tspan = (0.0, 10)
-p = [k_cp, k_pc, k_ei, a_ka, G]
+u0 = [0.0;0.0;1]
+tspan = (0.0, 90)
+p = [k_cp, k_pc, k_ei, a_ka]
 prob = ODEProblem(g,u0,tspan,p)
 sol = solve(prob)[2:end]
 plot(sol)
