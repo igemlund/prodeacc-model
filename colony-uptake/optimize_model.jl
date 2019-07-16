ac0951a2d000b0608f7785e37b4022d996c517ac
Arvid Larsson <arvla626@student.liu.se>
2019-07-16 11:02:19 +0200

    Slight imporve

--- colony-uptake/optimize_model.jl
+++ colony-uptake/optimize_model.jl
@@ -18,18 +18,19 @@ k_init = [V_max, k_m]
 # Generated data using other constants
 t = 0.0:1:5.0
 k_data = [V_max*5, k_m]
-init = ions_in_cytoplasm(t, k_init, 0.25)
-data1 = ions_in_cytoplasm(t, k_data, 0.25)
-data2 = ions_in_cytoplasm(t, k_data, 0.50)
-data3 = ions_in_cytoplasm(t, k_data, 0.125)
+init = ions_in_medium(t, k_init, 0.25)
+data1 = ions_in_medium(t, k_data, 0.25)
+data2 = ions_in_medium(t, k_data, 0.50)
+data3 = ions_in_medium(t, k_data, 0.125)
 
 # Cost function for optimizing model, using different concentrations
-f(x) = (loss_function(data1, ions_in_cytoplasm(t,x, 0.25))
-    + loss_function(data2, ions_in_cytoplasm(t,x,0.5))
-    + loss_function(data3, ions_in_cytoplasm(t,x,0.125)))
+f(x) = (loss_function(data1, ions_in_medium(t,x, 0.25))
+    + loss_function(data2, ions_in_medium(t,x,0.5))
+    + loss_function(data3, ions_in_medium(t,x,0.125)))
 
 # Optimize
-res = optimize(f, k_init, NelderMead())
+res = optimize(f, k_init, NelderMead(), Optim.options(  g_tol=1e-9,
+                                                        show_trace=true))
 k_opt = Optim.minimizer(res)
 
 # Plot
