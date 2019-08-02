7c33261211ef9027d37cd3148c4330e3651a5735
Arvid Larsson <arvla626@student.liu.se>
2019-08-02 10:36:19 +0200

    Improove solution handeling
    The solver now returns 'nothing' if it doesn't converge. This is usefull when running
    parameter optimization and sensitivity analasis.

--- colony-uptake/colony-uptake.jl
+++ colony-uptake/colony-uptake.jl
@@ -6,36 +6,35 @@ using ParameterizedFunctions
 using Plots
 # http://book.bionumbers.org/how-many-proteins-are-made-per-mrna-molecule/
 # Constants
-Av = 6.022e23                       # Avogrado's number
-V_cell = 8e-16                      # Cell volume   [l]
-V_gut = 1.2                         # Gut volume    [l]
-thickness = 10e-9                    # Membrane thickness [meters]
+const Av = 6.022e23                       # Avogrado's number
+const V_cell = 8e-16                      # Cell volume   [l]
+const V_gut = 1.2                         # Gut volume    [l]
 ions2mcg(ions, molarmass) = ions/Av*molarmass*1e6
 mcg2ions(mcg, molarmass) = mcg*1e-6/molarmass*Av
 
 # Rates
-k_tsl = 1*60*60                     # Translation rate [/h]
-k_rna_deg = 20.79                   # RNA degradation rate [/h]
-k_growth = log(2)/1                 # Growth rate, double every 30 min. [/h]
-k_pro_deg = 0.1          # Effective Protein degradation rate [/h]
-V_max = 500e-6 * Av                 # unknown [/h]
-k_m = 0.1 * Av / V_cell     # unknown [mol/m^3]
-k_CA = 1.0                          # unknown Accumulation protein capture rate
-k_CG = 1.0                          # unknonw Ions Cytoplasm --> Gut
+const k_tsl = 1*60*60                     # Translation rate [/h]
+const k_rna_deg = 20.79                   # RNA degradation rate [/h]
+const k_growth = log(2)/1                 # Growth rate, double every 30 min. [/h]
+const k_pro_deg = 0.1                     # Effective Protein degradation rate [/h]
+#const V_max = 500 * Av                    # unknown [/h]
+V_max = 1000*60*60                  # unknown [/h]
+#const k_m = 0.1e-4 * Av / V_cell   # unknown [mol/m^3]
+k_m = mcg2ions(0.75, 75)            # unknown [mol/m^3]
+k_CA = 0.1                          # unknown Accumulation protein capture rate
+k_CG = 0.0                          # unknonw Ions Cytoplasm --> Gut
 
 # Start conditions
-G0 = mcg2ions(0.25, 75) * V_gut     # Gut start amount
-N0 = 1e9                            # Bacteria start amount
-N_max = 1e13                        # Assumed max amount of prodeacc in human gut
+const G0 = mcg2ions(0.25, 75) * V_gut     # Gut start amount
+const N0 = 1e9                            # Bacteria start amount
+const N_max = 1e13                        # Assumed max amount of prodeacc in human gut
 
 # TODO: motvate this
-mRNAᵀ = 4
-mRNAᴬ = 4
-V_max = V_max * 1e-5
+const mRNAᴬ = 4
 bacteria_uptake = @ode_def begin
     # Colony
-    dN = k_growth * N - k_growth * (N^2) / N_max                                    # Number of bacteria in gut
-    dG =-V_max/(1+k_m/((G/V_gut-C/V_cell)/thickness))*N    # Number of ions in gut
+    dN = k_growth * N - k_growth * (N^2) / N_max    # Number of bacteria in gut
+    dG =-V_max/(1+k_m/(G/V_gut))*N                  # Number of ions in gut
 
     # Single Bacteria
     dAP_free = (k_tsl * mRNAᴬ           # Number of free accumulators
@@ -45,11 +44,11 @@ bacteria_uptake = @ode_def begin
     dAP_occupied = (k_CA * AP_free * C  # Number of occupied accumulators
         - dN / N * AP_occupied
         - k_pro_deg * AP_occupied)
-    dC = (V_max/(1+k_m/((V_gut-C/V_cell)/thickness))    # Number of ions in cytoplasm
+    dC = (V_max/(1+k_m/(G/V_gut))       # Number of ions in cytoplasm
         - k_CA * C * AP_free
         + k_pro_deg * AP_occupied
         - dN / N * C)
-end V_max k_m
+end V_max k_m k_CA
 
 function colony_uptake(t, K, c)
     u0 = zeros(5)
@@ -57,7 +56,12 @@ function colony_uptake(t, K, c)
     u0[2] = mcg2ions(c, 75)
     tspan = (0.0, t)
     prob = ODEProblem(bacteria_uptake,u0,tspan,K)
-    sol = solve(prob)
+    successful = true
+    try
+        sol = solve(prob)
+    catch
+        sol = nothing
+    end
 end
 
 function plot_init()
@@ -66,10 +70,13 @@ end
 
 function plot_model(sol)
     p1 = plot(sol, vars=[1], label="Number of cells", color="Red")
-    p2 = plot(sol, vars=[2], label="Ions in medium", color="Blue")
+    p2 = plot(sol, vars=[2], label="Ions in medium", color="Blue", ylims=(0, sol[2,1]))
     p3 = plot(sol, vars=[3,4,5], label=["Free accumulators" "Occupied accumulators" "Ions in cytoplasm"])
     sum_of_ions = sol[2,:].+sol[1,:].*(sol[4,:].+sol[5,:])
-    control = plot(sol.t, sum_of_ions, label="Total ions (shoud be constant)", color="Black")
+    control = plot(sol.t, sum_of_ions,
+        label="Total ions (shoud be constant)",
+        color="Black",
+        ylims=(0.9*sol[2,1], 1.1sol[2,1]))
     plot(p1, p2, p3, control, layout=(2,2))
 end
 
@@ -78,10 +85,10 @@ function plot_transport(G)
     plot(flux.(G))
 end
 
-K = [V_max, k_m]
+K = [V_max, k_m, k_CA]
 plotlyjs()
 plot()
 plot_init()
-sol = colony_uptake(50.0, K, 0.25)
+sol = colony_uptake(12.0, K, 0.25)
 plot_model(sol)
 #plot_transport(colony_uptake(18.0, K, 0.25)[2,:])
