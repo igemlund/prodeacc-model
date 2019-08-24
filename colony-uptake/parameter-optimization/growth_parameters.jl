4ac4dd7ef11827a88dd2502eac741a7efeb787ba
Arvid Larsson <arvla626@student.liu.se>
2019-08-24 17:02:50 +0200

    Growth prameter fit, works with real data

--- colony-uptake/parameter-optimization/growth_parameters.jl
+++ colony-uptake/parameter-optimization/growth_parameters.jl
@@ -1,15 +1,14 @@
 using CSV
 using Optim
 
-EcN = CSV.read("data/EcN.csv")
-EcNT = CSV.read("data/EcNT.csv")
-EcN_od = [EcN[:,1], EcN[:,5]]
-EcNT_od = [EcNT[:,1], EcNT[:,5]]
-deleteat!(EcN_od[1], 10)
-deleteat!(EcN_od[2], 10)
-scatter(EcN_od[1], EcN_od[2].*1e9)
+T1 = CSV.read("T1.csv")
+ind = [1, 5, 8]
+time = T1[1][ind]
+cfu = T1[13][ind]
 
-f(x)=loss_function(target_data(float.(EcN_od[1]), 1, [(:k_growth, x[1]), (:N_max, x[2])]), EcN_od[2].*1e9)
+scatter(time, cfu)
+
+f(x)=loss_function(target_data(float.(time), 1, [(:N0, cfu[1]), (:k_growth, x[1]), (:N_max, x[2]), (:lag_phase, x[3])]), cfu)
 options = Optim.Options(show_trace=true,
                         show_every=100,
                         iterations=8000,
@@ -18,14 +17,16 @@ options = Optim.Options(show_trace=true,
                         time_limit=1000)
 
 # k_growth, N_max
-upper = [log(2)/0.1, 3e10]
-lower = [log(2)/2, 1e10]
-k_init = [log(2)/1, 1e10]
+upper = [log(2)/0.1, 3e10, 4.0]
+lower = [log(2)/2, 1e5, 0.5]
+k_init = [log(2)/1, 1e6, 2.0]
 
 alg = ParticleSwarm(n_particles=6, lower=lower, upper=upper)
-alg = NelderMead()
+#alg = NelderMead()
 
 time = @elapsed res = optimize(f, k_init,alg, options)
-k_growth, N_max = res.minimizer
+k_growth, N_max, lag_phase = res.minimizer
 plotlyjs()
-plot!(colony_model(12, [(:k_growth, k_growth), (:N_max, N_max)]), vars=[1])
+sol = colony_model(12, [(:N0, cfu[1]),(:k_growth, k_growth), (:N_max, N_max), (:lag_phase, lag_phase)])
+sol(0)[1]
+plot!(sol, vars=[1])
