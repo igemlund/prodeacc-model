391e0890bf2369984adf6fbd64a2fb01b50efccb
Arvid Larsson <arvla626@student.liu.se>
2019-08-24 12:22:34 +0200

    Added lag phase support

--- colony-uptake/colony-uptake.jl
+++ colony-uptake/colony-uptake.jl
@@ -4,6 +4,7 @@ using DifferentialEquations
 using ODEInterfaceDiffEq
 using ParameterizedFunctions
 using Plots
+pyplot()
 
 # Constants
 const Av = 6.022e23           # Avogrado's number
@@ -33,7 +34,7 @@ bacteria_uptake = @ode_def begin
         + k_pro_deg * AP_occupied
         - dN/N * C)
     dmRNA = tsc - k_rna_deg * mRNA
-end k_growth N_max k_tsl tsc k_rna_deg k_pro_deg V_max k_m k_CA k_CG N0 G0 c
+end k_growth N_max k_tsl tsc k_rna_deg k_pro_deg V_max k_m k_CA k_CG N0 G0 c lag ind
 
 function colony_model(t, symbols::Array{}=[])
     p = ([(:k_growth, log(2)/1)         # Growth rate, double every 30 min. [/h]
@@ -47,11 +48,15 @@ function colony_model(t, symbols::Array{}=[])
     (:k_CA, 0.1)                        # unknown Accumulation protein capture rate
     (:k_CG, 0.01)                       # unknonw Ions Cytoplasm --> Gut
     (:N0, 1e9)                          # Number of cells at start
-    (:G0, mcg2ions(0.25, 75)*V_gut)])     # Gut start amount
+    (:G0, mcg2ions(0.25, 75)*V_gut)     # Gut start amount
+    (:lag_phase, 2.0)
+    (:induced_time, 4.0)])
 
+    # Replace values
     k = map(x -> x[2], p)
     for s in symbols k[findfirst(x -> x[1] === s[1], p)] = s[2] end
 
+    # Set initial condidions
     u0 = zeros(7)
     u0[1] = k[11]
     u0[2] = k[12]
@@ -63,6 +68,8 @@ function colony_model(t, symbols::Array{}=[])
 
     prob = ODEProblem(bacteria_uptake,u0,tspan,k)
     sol = solve(prob, isoutofdomain=(u,p,t) -> any(x -> x < 0, u))
+    sol.t[2:end] .+= 4.0
+    sol
 end
 
 function plot_init()
@@ -76,11 +83,11 @@ function plot_model(sol)
     p2 = plot(sol, vars=[2],
         label="Ions in medium",
         color="Blue",
-        yscale=:log10)
+        yscale=:none)
     p3 = plot(sol,
         vars=[3,4,5],
         label=["Transporters" "Free accumulators" "Occupied accumulators"],
-        yscale=:log10)
+        yscale=:none)
     sum_of_ions = sol[2,:].+sol[1,:].*(sol[5,:].+sol[6,:])
     control = plot(sol.t,sum_of_ions,
         label="Total ions (shoud be constant)",
@@ -91,5 +98,6 @@ function plot_model(sol)
 end
 
 sol = colony_model(20.0, [(:N0, 1e10)])
+sol
 plot_init()
 plot_model(sol)
