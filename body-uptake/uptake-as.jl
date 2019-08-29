81dcf8254e77657d0816f754e0dc9d200e429bb0
Arvid Larsson <arvla626@student.liu.se>
2019-08-29 12:57:49 +0200

    constants are constants

--- body-uptake/uptake-as.jl
+++ body-uptake/uptake-as.jl
@@ -4,138 +4,138 @@ using DifferentialEquations
 using ParameterizedFunctions
 using IterableTables
 
-Q_gi = 1440  #Blood flow to GI tract (l/day)
-Q_liv = 446.4 #Blood flow to liver (l/day)
-Q_kid = 1440 #Blood flow to kidney (L/day)
-Q_skin = 374.4 #Blood flow to skin (l/day)
-Q_lung = 7488 #Blood flow to lung (L/day)
-Q_mus = 2592 #Blood flow to muscle (L/day)
-Q_hrt = 288 #Blood flow to heart (L/day)
-Q_br = 907.2 #Blood flow to brain (L/day)
+const Q_gi = 1440  #Blood flow to GI tract (l/day)
+const Q_liv = 446.4 #Blood flow to liver (l/day)
+const Q_kid = 1440 #Blood flow to kidney (L/day)
+const Q_skin = 374.4 #Blood flow to skin (l/day)
+const Q_lung = 7488 #Blood flow to lung (L/day)
+const Q_mus = 2592 #Blood flow to muscle (L/day)
+const Q_hrt = 288 #Blood flow to heart (L/day)
+const Q_br = 907.2 #Blood flow to brain (L/day)
 
-V_gi = 1.2 #Volume of GI tract (L)
-V_liv = 1.82 #Volume of liver (L)
-V_kid = 0.28 #Volume of kidney (L)
-V_skin = 2.6 #Volume of skin (L)
-V_lung = 0.56 #Volume of lung (L)
-V_blood = 5.53 #Volume of blood (L)
-V_mus = 55.5 #Volume of muscle (L)
-V_hrt = 0.35 #Volume of heart (L)
-V_br = 1.4 #Volume of brain (L)
+const V_gi = 1.2 #Volume of GI tract (L)
+const V_liv = 1.82 #Volume of liver (L)
+const V_kid = 0.28 #Volume of kidney (L)
+const V_skin = 2.6 #Volume of skin (L)
+const V_lung = 0.56 #Volume of lung (L)
+const V_blood = 5.53 #Volume of blood (L)
+const V_mus = 55.5 #Volume of muscle (L)
+const V_hrt = 0.35 #Volume of heart (L)
+const V_br = 1.4 #Volume of brain (L)
 
 #Inorganic patition coefficient
-P_gi3 = 8.3 #Gi tract/blood partition coefficient for As(III) (no unit)
-P_gi5 = 2.7 #Gi tract/blood partition coefficient for As(V) (no unit)
-P_liv3 = 16.5 #Same as above but for liver
-P_liv5 = 15.8
-P_kid3 = 20 #Same as above but for kidney
-P_kid5 = 8.3
-P_skin3 = 7.4 #Same as above but for skin
-P_skin5 = 7.9
-P_lung3 = 6.7 #Same as above but for lung
-P_lung5 = 2.1
-P_mus3 = 7.4 #Same as above but for muscle
-P_mus5 = 7.9
-P_hrt3 = 7.4 #Same as above but for heart
-P_hrt5 = 7.9
-P_br3 = 2.4 #Same as above but for brain
-P_br5 = 2.4
+const P_gi3 = 8.3 #Gi tract/blood partition coefficient for As(III) (no unit)
+const P_gi5 = 2.7 #Gi tract/blood partition coefficient for As(V) (no unit)
+const P_liv3 = 16.5 #Same as above but for liver
+const P_liv5 = 15.8
+const P_kid3 = 20 #Same as above but for kidney
+const P_kid5 = 8.3
+const P_skin3 = 7.4 #Same as above but for skin
+const P_skin5 = 7.9
+const P_lung3 = 6.7 #Same as above but for lung
+const P_lung5 = 2.1
+const P_mus3 = 7.4 #Same as above but for muscle
+const P_mus5 = 7.9
+const P_hrt3 = 7.4 #Same as above but for heart
+const P_hrt5 = 7.9
+const P_br3 = 2.4 #Same as above but for brain
+const P_br5 = 2.4
 
 #Organic partition coefficient (MMA and DMA are two forms of organic arsenic)
 #partition coefficient is the ratio of concentrations of a compound in a mixture of two immiscible phases at equilibrium
-P_giM = 2.2 #GI tract/blood partition coefficient for MMA ((GI/V_gi)/(B/V_blood))
-P_giN = 2.1 #GI tract/blood partition coefficient for DMA
-P_livM = 3.3 #Same as above but for liver
-P_livN = 3.3
-P_kidM = 100 #Same as above but for kidney
-P_kidN = 1
-P_skinM = 2.61 #Same as above but for skin
-P_skinN = 2.4
-P_lungM = 1.3 #Same as above but for skin
-P_lungN = 1.3
-P_musM = 2.61 #Same as above but for muscle
-P_musN = 2.4
-P_hrtM = 2.61 #Same as above but for heart
-P_hrtN = 2.4
-P_brM = 2.2 #Same as above but for brain
-P_brN = 3.3
+const P_giM = 2.2 #GI tract/blood partition coefficient for MMA ((GI/V_gi)/(B/V_blood))
+const P_giN = 2.1 #GI tract/blood partition coefficient for DMA
+const P_livM = 3.3 #Same as above but for liver
+const P_livN = 3.3
+const P_kidM = 100 #Same as above but for kidney
+const P_kidN = 1
+const P_skinM = 2.61 #Same as above but for skin
+const P_skinN = 2.4
+const P_lungM = 1.3 #Same as above but for skin
+const P_lungN = 1.3
+const P_musM = 2.61 #Same as above but for muscle
+const P_musN = 2.4
+const P_hrtM = 2.61 #Same as above but for heart
+const P_hrtN = 2.4
+const P_brM = 2.2 #Same as above but for brain
+const P_brN = 3.3
 
 #calculate absorption coefficient (A_gi) from P_gi, A_gi is the percentage of amount of arsenic being obsorbed to blood
-A_gi3 = (V_blood/V_gi)/(P_gi3 + (V_blood/V_gi))
-A_gi5 = (V_blood/V_gi)/(P_gi5 + (V_blood/V_gi))
-A_giM = (V_blood/V_gi)/(P_giM + (V_blood/V_gi))
-A_giN = (V_blood/V_gi)/(P_giN + (V_blood/V_gi))
+const A_gi3 = (V_blood/V_gi)/(P_gi3 + (V_blood/V_gi))
+const A_gi5 = (V_blood/V_gi)/(P_gi5 + (V_blood/V_gi))
+const A_giM = (V_blood/V_gi)/(P_giM + (V_blood/V_gi))
+const A_giN = (V_blood/V_gi)/(P_giN + (V_blood/V_gi))
 
-eF = 0.03 #faecal excretion rate (/day)
-eB = 0.43 #Biliary excretion rate (/day)
-eUᵢ = 100.8 #Urinary excretion rate for As3 and As5 (/day)
-eUₘ = 432 #Urinary excretion rate for MMA (/day)
-eUₙ = 187.2 #Urinary excretion rate for DMA (/day)
+const eF = 0.03 #faecal excretion rate (/day)
+const eB = 0.43 #Biliary excretion rate (/day)
+const eUᵢ = 100.8 #Urinary excretion rate for As3 and As5 (/day)
+const eUₘ = 432 #Urinary excretion rate for MMA (/day)
+const eUₙ = 187.2 #Urinary excretion rate for DMA (/day)
 
 
 #Rate constant of As(III) (/day)
-k_GB₃ = Q_gi/(V_gi*P_gi3) #from GI tract to blood
-k_LiB₃ = Q_liv/(V_liv*P_liv3) #from liver to blood
-k_KB₃ = Q_kid/(V_kid*P_kid3) #from kidney to blood
-k_SB₃ = Q_skin/(V_skin*P_skin3) #from skin to blood
-k_LuB₃ = Q_lung/(V_lung*P_lung3) #from lung to blood
-k_MB₃ = Q_mus/(V_mus*P_mus3)
-k_HB₃ = Q_hrt/(V_hrt*P_hrt3)
-k_BrB₃ = Q_br/(V_br*P_br3)
+const k_GB₃ = Q_gi/(V_gi*P_gi3) #from GI tract to blood
+const k_LiB₃ = Q_liv/(V_liv*P_liv3) #from liver to blood
+const k_KB₃ = Q_kid/(V_kid*P_kid3) #from kidney to blood
+const k_SB₃ = Q_skin/(V_skin*P_skin3) #from skin to blood
+const k_LuB₃ = Q_lung/(V_lung*P_lung3) #from lung to blood
+const k_MB₃ = Q_mus/(V_mus*P_mus3)
+const k_HB₃ = Q_hrt/(V_hrt*P_hrt3)
+const k_BrB₃ = Q_br/(V_br*P_br3)
 
 #Rate constant of As(V) (/day)
-k_GB₅ = Q_gi/(V_gi*P_gi5) #from GI tract to blood
-k_LiB₅ = Q_liv/(V_liv*P_liv5) #from liver to blood
-k_KB₅ = Q_kid/(V_kid*P_kid5) #from kidney to blood
-k_SB₅ = Q_skin/(V_skin*P_skin5) #from skin to blood
-k_LuB₅ = Q_lung/(V_lung*P_lung5) #from lung to blood
-k_MB₅ = Q_mus/(V_mus*P_mus5)
-k_HB₅ = Q_hrt/(V_hrt*P_hrt5)
-k_BrB₅ = Q_br/(V_br*P_br5)
+const k_GB₅ = Q_gi/(V_gi*P_gi5) #from GI tract to blood
+const k_LiB₅ = Q_liv/(V_liv*P_liv5) #from liver to blood
+const k_KB₅ = Q_kid/(V_kid*P_kid5) #from kidney to blood
+const k_SB₅ = Q_skin/(V_skin*P_skin5) #from skin to blood
+const k_LuB₅ = Q_lung/(V_lung*P_lung5) #from lung to blood
+const k_MB₅ = Q_mus/(V_mus*P_mus5)
+const k_HB₅ = Q_hrt/(V_hrt*P_hrt5)
+const k_BrB₅ = Q_br/(V_br*P_br5)
 
 #Rate constant of MMA (/day)
-k_GBₘ = Q_gi/(V_gi*P_giM) #from GI tract to blood
-k_LiBₘ = Q_liv/(V_liv*P_livM) #from liver to blood
-k_KBₘ = Q_kid/(V_kid*P_kidM) #from kidney to blood
-k_SBₘ = Q_skin/(V_skin*P_skinM) #from skin to blood
-k_LuBₘ = Q_lung/(V_lung*P_lungM) #from lung to blood
-k_MBₘ = Q_mus/(V_mus*P_musM)
-k_HBₘ = Q_hrt/(V_hrt*P_hrtM)
-k_BrBₘ = Q_br/(V_br*P_brM)
+const k_GBₘ = Q_gi/(V_gi*P_giM) #from GI tract to blood
+const k_LiBₘ = Q_liv/(V_liv*P_livM) #from liver to blood
+const k_KBₘ = Q_kid/(V_kid*P_kidM) #from kidney to blood
+const k_SBₘ = Q_skin/(V_skin*P_skinM) #from skin to blood
+const k_LuBₘ = Q_lung/(V_lung*P_lungM) #from lung to blood
+const k_MBₘ = Q_mus/(V_mus*P_musM)
+const k_HBₘ = Q_hrt/(V_hrt*P_hrtM)
+const k_BrBₘ = Q_br/(V_br*P_brM)
 
 #Rate constant of DMA (/day)
-k_GBₙ = Q_gi/(V_gi*P_giN) #from GI tract to blood
-k_LiBₙ = Q_liv/(V_liv*P_livN) #from liver to blood
-k_KBₙ = Q_kid/(V_kid*P_kidN) #from kidney to blood
-k_SBₙ = Q_skin/(V_skin*P_skinN) #from skin to blood
-k_LuBₙ = Q_lung/(V_lung*P_lungN) #from lung to blood
-k_MBₙ = Q_mus/(V_mus*P_musN)
-k_HBₙ = Q_hrt/(V_hrt*P_hrtN)
-k_BrBₙ = Q_br/(V_br*P_brN)
+const k_GBₙ = Q_gi/(V_gi*P_giN) #from GI tract to blood
+const k_LiBₙ = Q_liv/(V_liv*P_livN) #from liver to blood
+const k_KBₙ = Q_kid/(V_kid*P_kidN) #from kidney to blood
+const k_SBₙ = Q_skin/(V_skin*P_skinN) #from skin to blood
+const k_LuBₙ = Q_lung/(V_lung*P_lungN) #from lung to blood
+const k_MBₙ = Q_mus/(V_mus*P_musN)
+const k_HBₙ = Q_hrt/(V_hrt*P_hrtN)
+const k_BrBₙ = Q_br/(V_br*P_brN)
 
 #Rate constant from blood to other organs for both organic and inorganic arsenic
-k_BG = Q_gi/V_blood#from Blood to GI tract
-k_BLi = Q_liv/V_blood #from blood to liver
-k_BK = Q_kid/V_blood #from blood to kidney
-k_BS = Q_skin/V_blood #from blood to skin
-k_BLu = Q_lung/V_blood #from blood to lung
-k_BM = Q_mus/V_blood
-k_BH = Q_hrt/V_blood
-k_BBr = Q_br/V_blood
+const k_BG = Q_gi/V_blood#from Blood to GI tract
+const k_BLi = Q_liv/V_blood #from blood to liver
+const k_BK = Q_kid/V_blood #from blood to kidney
+const k_BS = Q_skin/V_blood #from blood to skin
+const k_BLu = Q_lung/V_blood #from blood to lung
+const k_BM = Q_mus/V_blood
+const k_BH = Q_hrt/V_blood
+const k_BBr = Q_br/V_blood
 
 #Reduction and Oxidation of inorganic arsenic
-R_Tis = 32.88 #As(V) reduction in tissues (/day)
-R_K = 42 #As(V) reduction in kidney (/day)
+const R_Tis = 32.88 #As(V) reduction in tissues (/day)
+const R_K = 42 #As(V) reduction in kidney (/day)
 O_Tis = 43.92 #As(III) oxidation is tissues (/day)
 
 #Metabolism constant for methylation of arsenic
-M_Li3M = 763 #from As3 to MMA in the liver (μmol/day)
-M_Li3N = 2880 #from As3 to DMA in the liver (μmol/day)
-M_LiMN = 950.4 #from MMA to DMA (μmol/day)
-M_K3M = 305.2 #from As3 to MMA in the kidney (μmol/day)
-M_K3N = 1152 #from As3 to DMA in the kidney (μmol/day)
-M_KMN = 380.16 #from MMA to DMA in the kidney (μmol/day)
-M = 3 #another metabolism constant (μmol/L), all of them have value 3 so it can be combined into one variable
+const M_Li3M = 763 #from As3 to MMA in the liver (μmol/day)
+const M_Li3N = 2880 #from As3 to DMA in the liver (μmol/day)
+const M_LiMN = 950.4 #from MMA to DMA (μmol/day)
+const M_K3M = 305.2 #from As3 to MMA in the kidney (μmol/day)
+const M_K3N = 1152 #from As3 to DMA in the kidney (μmol/day)
+const M_KMN = 380.16 #from MMA to DMA in the kidney (μmol/day)
+const M = 3 #another metabolism constant (μmol/L), all of them have value 3 so it can be combined into one variable
 #G₅ = 1.67
 
 
