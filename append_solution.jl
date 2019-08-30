81fcff2dbe40cfd6f47cffab26de31b99bd254a5
Arvid Larsson <arvla626@student.liu.se>
2019-08-30 11:30:01 +0200

    Adjust append

--- append_solution.jl
+++ append_solution.jl
@@ -5,8 +5,8 @@ function sol_append(sol1, sol2)
     if sol1.dense || sol2.dense
         @warn "Option dense=true, set it to false"
     end
-    append!(sol1.u, sol2.u[2,:])
-    append!(sol1.t, sol2.t[2,:])
+    append!(sol1.u, sol2.u)
+    append!(sol1.t, sol2.t)
     append!(sol1.alg_choice, sol2.alg_choice[2,:])
     return sol1
 end
