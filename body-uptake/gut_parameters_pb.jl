8ed6b5a759a9f1ee481f5592e34513923f5009b1
Arvid Larsson <arvla626@student.liu.se>
2019-08-30 16:09:48 +0200

    Pb kinteic rates for gut

--- body-uptake/gut_parameters_pb.jl
+++ body-uptake/gut_parameters_pb.jl
@@ -10,8 +10,8 @@ function loss_function(k)
     dose = 105e-3
     t = 1:2:days
     try
-        sol = pb_body_uptake(days, dose)(t)
-        sol_mod = pb_body_uptake_modified(days, dose)(t)
+        sol = pb_body_uptake(days, dose, false)(t)
+        sol_mod = pb_body_uptake(days, dose, true)(t)
         return sum(abs.(sol .- sol_mod))/length(sol)
     catch e
         @warn "Did not converge."
@@ -34,10 +34,10 @@ k_init = [-log(A_gi), -log(A_gi)]
 alg = ParticleSwarm(lower=l, upper=u,n_particles=6)
 time = @elapsed res = optimize(loss_function, k_init, alg, options)
 k_GB, k_GE = res.minimizer
+display(k_GE)
 
-sol1 = pb_body_uptake(400, 205e-2)
-sol2 = pb_body_uptake_modified(400, 205e-2)
-
+sol1 = pb_body_uptake(400, 205e-2, false)
+sol2 = pb_body_uptake(400, 205e-2, true)
 plot(sol1, vars=[2,3])
 plot!(sol2, vars=[2,3], linestyle=:dash)
 plot(sol2, vars=[1], xlims=[0, 1])
