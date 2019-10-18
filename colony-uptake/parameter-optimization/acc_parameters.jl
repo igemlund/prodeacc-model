a535f2be8855d5e9fea30107d6bda3f914e10390
Arvid Larsson <arvla626@student.liu.se>
2019-10-18 16:39:16 +0200

    Update acc parameters

--- colony-uptake/parameter-optimization/acc_parameters.jl
+++ colony-uptake/parameter-optimization/acc_parameters.jl
@@ -3,8 +3,8 @@ using Optim
 include("../colony-uptake.jl")
 include("../optimize_model.jl")
 
-c0 = sum([0.52, 1.1, 1])/3*1e-3/74.9*Av
-c1 = 0.6 * c0
+c0 = sum([0.52, 1.1, 1])/3*1e-3/70.9*Av
+c1 = 0.8 * c0
 c = [c0, c1]
 t = [0.0, 2.5]
 
@@ -16,6 +16,7 @@ f(x)=loss_function(target_data(t, 2,
     (:k_CA, x[3]),
     (:TP0, 1000),
     (:AP0, 1000)]), c)
+
 options = Optim.Options(show_trace=true,
                         show_every=100,
                         iterations=8000,
@@ -24,10 +25,10 @@ options = Optim.Options(show_trace=true,
                         time_limit=1000)
 
 # k_growth, N_max
-V_max, k_m, k_CA = [100, 1e6, 1e-1]
+V_max, k_m, k_CA = [40.1, 1e-4, 1e-1]
 k_init =    [V_max, k_m, k_CA]
-k_upper = [1e3, 1e13, 3]
-k_lower =  [1e2, 1e4, 0.1]
+k_upper = [1e3, 1e1, 3]
+k_lower =  [1e1, 1e-4, 0.1]
 
 alg = ParticleSwarm(n_particles=5, lower=k_lower, upper=k_upper)
 #alg = NelderMead()
@@ -39,8 +40,8 @@ plot(sol, vars=[2],
     label="Modeled concentration",
     xlims=[0, 3.0],
     linewidth=4,
-    ylabel="Number of Pb ions",
+    ylabel="Number of As ions",
     xlabel="hours")
 scatter!(t , c, label="Measured concentration")
-savefig("fit.svg")
-savefig("fit.png")
+savefig("fit_as.svg")
+savefig("fit_as.png")
